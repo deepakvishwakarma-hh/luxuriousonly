7aae836096e63a08eeb63f0f413651253999a0f9
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2025-12-05 10:57:33 +0530

    addon : review

--- backend/public/admin/assets/chunk-HKVC5HU4-D8oPFzDM.js
+++
@@ -1,1 +0,0 @@
-import{aT as i}from"./index-BtRdIgGu.js";var a=i("product","*categories,*shipping_profile,-variants");export{a as P};
---
+++ backend/public/admin/assets/chunk-HKVC5HU4-D9aNsmDt.js
@@ -0,0 +1,1 @@
+import{aU as i}from"./index-DByUhO00.js";var a=i("product","*categories,*shipping_profile,-variants");export{a as P};
