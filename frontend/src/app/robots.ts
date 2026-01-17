a4be96f126faa5b173d3f1dacf16002bee8aad9c
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2026-01-17 12:18:58 +0530

    Update route imports in TypeScript configuration and modify robots file for improved routing
    - Changed the import path in next-env.d.ts to point to the correct development routes type file.
    - Updated the robots.ts file to replace the "/forget-password/" route with "/wishlist/" for better route management.

--- frontend/src/app/robots.ts
+++ frontend/src/app/robots.ts
@@ -15,7 +15,7 @@ export default function robots(): MetadataRoute.Robots {
           "/api/",
           "/order/",
           "/test/",
-          "/forget-password/",
+          "/wishlist/",
         ],
       },
     ],
